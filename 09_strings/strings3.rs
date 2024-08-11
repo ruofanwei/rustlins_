3ffb1171567b40d0bc29e4620ac046980d1b2ac4
ruofan <px732amgo81sp8@gmail.com>
2024-08-11 21:52:15 +0800

    string

--- 09_strings/strings3.rs
+++ 09_strings/strings3.rs
@@ -1,17 +1,24 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{} world!", input)
+    // 或者使用 input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
+    trim_me("Hello!     ");
+    compose_me("Hello");
+    replace_me("I love to look at cars");
 }
 
 #[cfg(test)]
